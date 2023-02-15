6947055c04c82f43b12ac4874b07f918b835e70e
David-Inkheart <okoliedave1@gmail.com>
2023-02-15 18:30:26 +0000

    bug fixes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" The app module"""
+"""The app module to handle instantiation and
+registration of blueprint"""
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
 import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -16,7 +17,7 @@ def teardown_appcontext(exception):
     storage.close()
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" The index module """
+""" The index module for the App """
 from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False)
 def status():
     """ Returns the HTTP status """
     return jsonify({"status": "OK"})
