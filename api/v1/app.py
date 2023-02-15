88ce4a1aba43688a3e3e5deaa51fcd75f9f79f28
xz <odufeso1@gmail.com>
2023-02-15 15:21:35 +0100

    Bug fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -7,9 +8,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
 
 if __name__ = "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
