2de751c21dea7dd2f4e3049fd2d8bbe2ff4246c4
xz <odufeso1@gmail.com>
2023-02-16 23:21:31 +0100

    Bug Fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,7 +2,7 @@
 """Module for cities view"""
 
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.city import City
 from models.state import State
